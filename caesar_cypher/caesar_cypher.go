3c17010962b93793ddb5a59673c58d85bfd3a4f1
Caio Tony <caiotony16@gmail.com>
2021-08-19 14:04:14 +0100

    feat: add encoder function and segregate encoder and decoder at diferent functions

--- caesar_cypher/caesar_cypher.go
+++ caesar_cypher/caesar_cypher.go
@@ -6,16 +6,45 @@ import (
 )
 func main() {
 	fmt.Println("Welcome");
-	result := caesar_cypher("L fdph, L vdz, L frqtxhuhg", 3)
-	fmt.Println(result)
+	decoded_text := decoder("L fdph, L vdz, L frqtxhuhg", 3)
+	encoded_text := encoder(decoded_text, 3);
+
+
+	if test(encoded_text, decoded_text) {
+		fmt.Println("[OK] ALL TESTS PASSED")
+	}else {
+		fmt.Println("[ERROR] TESTS FAILED")
+	}
+	
 }
 
-func caesar_cypher(text string, hop uint8) string {
+func decoder(encoded_text string, hop uint8) string {
   decoded_text := strings.Map(func(r rune) rune {
 		if r == ',' || r == ' ' {
 			return r
 		}
 		return r-rune(hop)
-	}, text)
+	}, encoded_text)
 	return decoded_text;
+}
+
+func encoder(decoded_text string, hop uint8) string {
+	encoded_text := strings.Map(func(r rune) rune {
+		if r == ',' || r == ' ' {
+			return r
+		}
+		return r+rune(hop)
+	}, decoded_text)
+	return encoded_text
+}
+
+func test(encoded_text string, decoded_text string) bool{
+	truthy := 0
+	if encoded_text == "L fdph, L vdz, L frqtxhuhg" {
+		truthy++
+	} 
+	if decoded_text == "I came, I saw, I conquered" {
+		truthy++
+	}
+	return truthy == 2;
 }
